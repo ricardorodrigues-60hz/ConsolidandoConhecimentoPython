af288467a99a95c215a4b88db8368a69fbf3086f
Ricardo Rodrigues <101021873+ricardorodrigues-60hz@users.noreply.github.com>
2026-05-06 15:06:02 -0300

    TeladeLogin changes

--- 02_POO/Python/TelaDeLogin/src/main.py
+++ 02_POO/Python/TelaDeLogin/src/main.py
@@ -47,93 +47,64 @@ class loginWindow(Screen):
             MainWindow.current = self.email.text
             self.reset()
             sm.current = "main"
-
+            
         else:
             invalidLogin()
 
 
     def createBtn(self):
-
         self.reset()
-
         sm.current = "create"
 
 
     def reset(self):
-
         self.email.text = ""
-
         self.password.text = ""
 
 
 class MainWindow(Screen):
-
     n = ObjectProperty(None)
-
     created = ObjectProperty(None)
-
     email = ObjectProperty(None)
-
     current = ""
 
 
     def logOut(self):
-
         sm.current = "login"
 
 
     def on_enter(self, *args):
-
         password, name, created = db.get_user(self.current)
-
         self.n.text = "Account Name: " + name
-
         self.email.text = "Email: " + self.current
-
         self.created.text = "Created On: " + created
 
 class WindowManager (ScreenManager):
-
     pass
 
 
 def invalidLogin():
-
     pop = Popup(title='Invalid Login', content=Label(text= 'Invalid username or password.'), size_hint=(None, None), size=(400, 400))
-
     pop.open()
 
-
 def invalidForm():
-
     pop = Popup(title='Invalid Form', content=Label(text='Please fill in all inputs with valid information.'), size_hint=(None, None), size =(400, 400))
     pop.open()
 
-
 kv = Builder.load_file("my.kv")
-
-
 sm = WindowManager()
-
 db = DataBase("users.txt")
+screens = [loginWindow(name="login"), createAccountWindow(name="create"),MainWindow(name="main")]
 
-
-screens = [LoginWindow(name="login"), CreateAccountWindow(name="create"),MainWindow(name=" main")]
-
-for Screen in screens:
-
+for screen in screens:
     sm.add_widget(screen)
-
-
     sm.current = "login"
 
 
 class MyMainapp(App):
-
     def build(self):
-
         return sm
 
 
 if __name__ == "__main__":
-    MyMainApp().run()
+    MyMainapp().run()
